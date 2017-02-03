4015fea0585eab16255a11835e2d897ce23c9418
Zhicheng Wang <zcwang@thoughtworks.com>
2017-02-03 17:02:25 +0800

    Initial Commit

---
+++ models/webpack-configs/development.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.getDevConfig = function (wco) {
+    return {};
+};
+//# sourceMappingURL=/Users/twer/dev/sdk/angular-cli/packages/@angular/cli/models/webpack-configs/development.js.map
